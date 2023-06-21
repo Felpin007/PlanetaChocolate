92bc1a53f50918d116246cc1b680077b753a825d
Felipe <100373987+Felpin007@users.noreply.github.com>
2023-06-21 16:55:04 -0300

    Add files via upload

---
+++ ChocoPLanet/script.js
@@ -0,0 +1,16 @@
+let p1 = document.getElementById('p1');
+
+p1.addEventListener('click', teste);
+
+// puxar todos os valores de uma origem
+const j1 = `{ 
+    "product" : {
+        "name" : "bis",
+        "price" : "15",
+        "quant" :                      
+    }
+}` 
+
+
+// foco em manipulação de json
+sp = []
